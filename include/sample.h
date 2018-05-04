343a177848e45b35a78760c5f1ea67a9c1075f3b
Kenan <kenan.lejosne@gmail.com>
2018-05-04 06:02:13 +0200

    initial commit

---
+++ include/sample.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//Sample class just to have something in the include folder therefore
+//being able to push it
+class Sample
+{
+  int a;
+};
---
+++ src/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "sample.h"
+
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
