79edd2638cd83f2a2f4fdf35c6ee346c1288c4cc
Kenan <kenan.lejosne@gmail.com>
2018-05-06 04:12:24 +0200

    Options: adds options described in the README to the struct

--- include/options.h
+++ include/options.h
@@ -6,6 +6,10 @@ struct Options
 {
   Options();
   Options(char** argv, int argc);
+  bool                silent    {false};
   int                 is_server {-1};
   int                 port      {-1};
+  std::string         addr      {"None"};
+  std::string         login     {"defaultuser"};
+  std::string         pass      {""};
 };
